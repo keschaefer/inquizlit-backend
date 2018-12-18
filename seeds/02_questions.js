7c9976e644d47ede8feb6c182e6471749f33603a
grantfogle <grantjfogle@gmail.com>
2018-12-18 11:39:41 -0700

    updating seed data for questions and answers

--- seeds/02_questions.js
+++ seeds/02_questions.js
@@ -1,13 +1,73 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('questions').del()
     .then(function () {
       // Inserts seed entries
       return knex('questions').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {
+          question: 'What sparked your interest in web development?',
+          tag: 'cultural',
+          company: 'ABC Corp',
+          interviewer: 'Kathy',
+          upvotes: 2,
+          downvotes: 1,
+          user_id: 1
+        },
+        {
+          question: 'What is W3C and why is it important?',
+          tag: 'cultural',
+          company: 'DEF Inc.',
+          interviewer: 'Bryant',
+          upvotes: 5,
+          downvotes: 2,
+          user_id: 2
+        },
+        {
+          question: 'Explain how you optimize and reduce web application load time.',
+          tag: 'cultural',
+          company: 'GHI Company',
+          interviewer: 'Gerald',
+          upvotes: 22,
+          downvotes: 7,
+          user_id: 2
+        },
+        {
+          question: 'In what programming languages are you proficient?',
+          tag: 'cultural',
+          company: 'Haters Inc.',
+          interviewer: 'Jenn',
+          upvotes: 5,
+          downvotes: 2,
+          user_id: 3
+        },
+        {
+          question: 'Describe the project you\'ve worked on that you\'re most proud of. What did you do that worked out particularly well?',
+          tag: 'cultural',
+          company: 'Getit Done',
+          interviewer: 'Julie',
+          upvotes: 12,
+          downvotes: 4,
+          user_id: 4
+        },
+        {
+          question: 'Describe the project you\'ve worked on that you\'re least proud of. What would you do differently?',
+          tag: 'cultural',
+          company: 'Stuff Company',
+          interviewer: 'Todd',
+          upvotes: 5,
+          downvotes: 2,
+          user_id: 3
+        },
+        {
+          question: 'What is your greatest weakness?',
+          tag: 'cultural',
+          company: 'Bro Inc.',
+          interviewer: 'Thadd',
+          upvotes: 15,
+          downvotes: 6,
+          user_id: 2
+        }
       ]);
     });
 };
--- seeds/03_answers.js
+++ seeds/03_answers.js
@@ -1,13 +1,59 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('table_name').del()
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {
+          answer: 'W3C is a worldwide organization developed to create standards for the web.',
+          upvotes: 3,
+          downvotes: 1,
+          user_id: 1,
+          question_id: 2
+        },
+        {
+          answer: 'I have always love the creative aspect of problem solving, web development is an avenue that allows me to channel that createive outlet.',
+          upvotes: 4,
+          downvotes: 0,
+          user_id: 2,
+          question_id: 1
+        },
+        {
+          answer: 'I honestly have no idea.',
+          upvotes: 42,
+          downvotes: 1,
+          user_id: 3,
+          question_id: 3
+        },
+        {
+          answer: 'JavaScript hero right here. Also sick with the python. Get up and Go. Java in my cup. C## later...',
+          upvotes: 8,
+          downvotes: 2,
+          user_id: 4,
+          question_id: 4
+        },
+        {
+          answer: 'I did a group project at school where we created an app. It took a bunch of our skills and was a ton of fun working and coding with others, golly jee I had fun!',
+          upvotes: 8,
+          downvotes: 2,
+          user_id: 3,
+          question_id: 5
+        },
+        {
+          answer: 'Honestly. I tried to make pad thai one time. Went to the store, spent 50 bucks on ingredients and everything, and i soaked the brown rice pasta, and then didn\'t boil it and put it in the pan and it butchered it.',
+          upvotes: 10,
+          downvotes: 3,
+          user_id: 4,
+          question_id: 6
+        },
+        {
+          answer: 'Getting out of bed in the morning. Really hate life and the world and it\'s just hard to get out and face it everyday.',
+          upvotes: 8,
+          downvotes: 2,
+          user_id: 4,
+          question_id: 4
+        }
       ]);
     });
 };
