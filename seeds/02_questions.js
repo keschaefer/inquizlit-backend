9e83511256edd7536ce321cd8b50f44a06c7adf2
jimbucktoo <jamesliang.g@gmail.com>
2019-01-15 12:04:36 -0700

    creation of new seed data however there is an issue when reseeding the data. correction requested.

--- seeds/02_questions.js
+++ seeds/02_questions.js
@@ -1,135 +1,81 @@
 exports.seed = function (knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('questions').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('questions').insert([
-        {
-          question: 'What sparked your interest in web development?',
-          tag: 'cultural',
-          company: 'ABC Corp',
-          interviewer: 'Kathy',
-          upvotes: 2,
-          downvotes: 1,
-          user_id: 1
-        },
-        {
-          question: 'What is W3C and why is it important?',
-          tag: 'cultural',
-          company: 'DEF Inc.',
-          interviewer: 'Bryant',
-          upvotes: 5,
-          downvotes: 2,
-          user_id: 2
-        },
-        {
-          question: 'Explain how you optimize and reduce web application load time.',
-          tag: 'cultural',
-          company: 'GHI Company',
-          interviewer: 'Gerald',
-          upvotes: 22,
-          downvotes: 7,
-          user_id: 2
-        },
-        {
-          question: 'In what programming languages are you proficient?',
-          tag: 'cultural',
-          company: 'Haters Inc.',
-          interviewer: 'Jenn',
-          upvotes: 5,
-          downvotes: 2,
-          user_id: 3
-        },
-        {
-          question: 'Describe the project you\'ve worked on that you\'re most proud of. What did you do that worked out particularly well?',
-          tag: 'cultural',
-          company: 'Getit Done',
-          interviewer: 'Julie',
-          upvotes: 12,
-          downvotes: 4,
-          user_id: 4
-        },
-        {
-          question: 'Describe the project you\'ve worked on that you\'re least proud of. What would you do differently?',
-          tag: 'cultural',
-          company: 'Stuff Company',
-          interviewer: 'Todd',
-          upvotes: 5,
-          downvotes: 2,
-          user_id: 3
-        },
-        {
-          question: 'What is your greatest weakness?',
-          tag: 'cultural',
-          company: 'Bro Inc.',
-          interviewer: 'Thadd',
-          upvotes: 15,
-          downvotes: 6,
-          user_id: 2
-        },
-        {
-          question: 'How do you find the missing number in a given integer array of 1 to 100?',
-          tag: 'algorithms',
-          company: 'Bro Inc.',
-          interviewer: 'Thadd',
-          upvotes: 8,
-          downvotes: 2,
-          user_id: 4
-        },
-        {
-          question: 'How do you check if two strings are anagrams of each other?',
-          tag: 'algorithms',
-          company: 'Boss Ladys Inc.',
-          interviewer: 'Sarah',
-          upvotes: 18,
-          downvotes: 1,
-          user_id: 3
-        },
-        {
-          question: 'What is polymorphism?',
-          tag: 'computer science',
-          company: 'Bro Inc.',
-          interviewer: 'Thad Castle',
-          upvotes: 13,
-          downvotes: 3,
-          user_id: 3
-        },
-        {
-          question: 'What is inheritance?',
-          tag: 'computer science',
-          company: 'Bro Inc.',
-          interviewer: 'Thad Castle',
-          upvotes: 3,
-          downvotes: 7,
-          user_id: 3
-        },
-        {
-          question: 'You have a goat, a horse, a tortoise, and a hare, which one does math the best and why?',
-          tag: 'riddles',
-          company: 'Best Company Ever.',
-          interviewer: 'Sammy Sam',
-          upvotes: 28,
-          downvotes: 8,
-          user_id: 3
-        },
-        {
-          question: 'If you take purple and mix it with yellow, what color do you get?',
-          tag: 'riddles',
-          company: 'Working Hard Inc.',
-          interviewer: 'Bobby Bob',
-          upvotes: 13,
-          downvotes: 2,
-          user_id: 4
-        },
-        {
-          question: 'Whats your favorite memory?',
-          tag: 'computer science',
-          company: 'Bro Inc.',
-          interviewer: 'Thadd',
-          upvotes: 15,
-          downvotes: 6,
-          user_id: 2
-        }
-      ]);
-    });
+    // Deletes ALL existing entries
+    return knex('questions').del()
+        .then(function () {
+            // Inserts seed entries
+            return knex('questions').insert([
+                {
+                    question: 'What are your weaknesses?',
+                    tag: 'cultural',
+                    company: 'Inquizlit Inc.',
+                    interviewer: 'Kathy',
+                    upvotes: 2,
+                    downvotes: 1,
+                    user_id: 1
+                },
+                {
+                    question: 'Do you work best alone or on a team?',
+                    tag: 'cultural',
+                    company: 'Inquizlit Inc.',
+                    interviewer: 'Bryant',
+                    upvotes: 5,
+                    downvotes: 2,
+                    user_id: 2
+                },
+                {
+                    question: 'What is MVC?',
+                    tag: 'computer science',
+                    company: 'Inquizlit Inc.',
+                    interviewer: 'Gerald',
+                    upvotes: 22,
+                    downvotes: 7,
+                    user_id: 2
+                },
+                {
+                    question: 'What is polymorphism?',
+                    tag: 'computer science',
+                    company: 'Inquizlit Inc.',
+                    interviewer: 'Jonathan',
+                    upvotes: 13,
+                    downvotes: 3,
+                    user_id: 3
+                },
+                {
+                    question: 'Write a function that will loop through a list of integers and print the index of each element after a 3 second delay.',
+                    tag: 'algorithms',
+                    company: 'Inquizlit Inc.',
+                    interviewer: 'Jenny',
+                    upvotes: 5,
+                    downvotes: 2,
+                    user_id: 3
+                },
+                {
+                    question: 'Write a simple function that returns a boolean indicating whether or not a string is a palindrome.',
+                    tag: 'algorithms',
+                    company: 'Inquizlit Inc.',
+                    interviewer: 'Julie',
+                    upvotes: 12,
+                    downvotes: 1,
+                    user_id: 4
+                },
+                {
+                    question: 'You have a goat, a horse, a tortoise, and a hare, which one does math the best and why?',
+                    tag: 'riddles',
+                    company: 'Inquizlit Inc.',
+                    interviewer: 'Martin',
+                    upvotes: 28,
+                    downvotes: 7,
+                    user_id: 3
+                },
+                {
+                    question: 'If you take purple and mix it with yellow, what color do you get?',
+                    tag: 'riddles',
+                    company: 'Inquizlit Inc.',
+                    interviewer: 'Madison',
+                    upvotes: 13,
+                    downvotes: 2,
+                    user_id: 4
+                }
+            ]);
+        });
 };
--- seeds/03_answers.js
+++ seeds/03_answers.js
@@ -1,59 +1,115 @@
 
 exports.seed = function (knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('answers').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('answers').insert([
-        {
-          answer: 'W3C is a worldwide organization developed to create standards for the web.',
-          upvotes: 3,
-          downvotes: 1,
-          user_id: 1,
-          question_id: 2
-        },
-        {
-          answer: 'I have always love the creative aspect of problem solving, web development is an avenue that allows me to channel that createive outlet.',
-          upvotes: 4,
-          downvotes: 0,
-          user_id: 2,
-          question_id: 1
-        },
-        {
-          answer: 'I honestly have no idea.',
-          upvotes: 42,
-          downvotes: 1,
-          user_id: 3,
-          question_id: 3
-        },
-        {
-          answer: 'JavaScript hero right here. Also sick with the python. Get up and Go. Java in my cup. C## later...',
-          upvotes: 8,
-          downvotes: 2,
-          user_id: 4,
-          question_id: 4
-        },
-        {
-          answer: 'I did a group project at school where we created an app. It took a bunch of our skills and was a ton of fun working and coding with others, golly jee I had fun!',
-          upvotes: 8,
-          downvotes: 2,
-          user_id: 3,
-          question_id: 5
-        },
-        {
-          answer: 'Honestly. I tried to make pad thai one time. Went to the store, spent 50 bucks on ingredients and everything, and i soaked the brown rice pasta, and then didn\'t boil it and put it in the pan and it butchered it.',
-          upvotes: 10,
-          downvotes: 3,
-          user_id: 4,
-          question_id: 6
-        },
-        {
-          answer: 'Getting out of bed in the morning. Really hate life and the world and it\'s just hard to get out and face it everyday.',
-          upvotes: 8,
-          downvotes: 2,
-          user_id: 4,
-          question_id: 4
-        }
-      ]);
-    });
+    // Deletes ALL existing entries
+    return knex('answers').del()
+        .then(function () {
+            // Inserts seed entries
+            return knex('answers').insert([
+            {
+                answer: 'Sometimes I lose drive in the middle of a project after the launch excitement has worn off. I combat that by breaking up the project into mini-milestones and celebrating them with my team. The original buzz gets recreated and it becomes obvious how crucial our work is.',
+                upvotes: 13,
+                downvotes: 2,
+                user_id: 1,
+                question_id: 1
+            },
+            {
+                answer: 'I’m such a perfectionist. I really have to hold back from being the best.',
+                upvotes: 4,
+                downvotes: 3,
+                user_id: 2,
+                question_id: 1
+            },
+            {
+                answer: 'I work best on a team.',
+                upvotes: 5,
+                downvotes: 2,
+                user_id: 1,
+                question_id: 2
+            },
+            {
+                answer: 'I love the speed and energy of teamwork, and I also like concentrated time to work alone and execute on projects.',
+                upvotes: 17,
+                downvotes: 4,
+                user_id: 2,
+                question_id: 2
+            },
+            {
+                answer: 'MVC stands for Model-View-Controller. It\'s a way to separate code into its own compartments.',
+                upvotes: 42,
+                downvotes: 1,
+                user_id: 3,
+                question_id: 3
+            },
+            {
+                answer: 'Polymorphism is the provision of a single interface to entities of different types or the use of a single symbol to represent multiple different types.',
+                upvotes: 35,
+                downvotes: 4,
+                user_id: 3,
+                question_id: 4
+            },
+            {
+                answer: 'Polymorphism is the condition of occurring in several different forms.',
+                upvotes: 9,
+                downvotes: 2,
+                user_id: 4,
+                question_id: 4
+            },
+            {
+                answer: 'const arr = [10, 12, 15, 21];for (var i = 0; i < arr.length; i++) {  setTimeout(function() {    console.log(\'The index of this number is: \' + i);  }, 3000);}',
+                upvotes: 8,
+                downvotes: 2,
+                user_id: 3,
+                question_id: 5
+            },
+            {
+                answer: 'const arr = [10, 12, 15, 21];for (var i = 0; i < arr.length; i++) {  setTimeout(function(i_local) {    return function() {      console.log(\'The index of this number is: \' + i_local);    }  }(i), 3000);}',
+                upvotes: 8,
+                downvotes: 2,
+                user_id: 3,
+                question_id: 5
+            },
+            {
+                answer: 'const arr = [10, 12, 15, 21];for (let i = 0; i < arr.length; i++) {  setTimeout(function() {    console.log(\'The index of this number is: \' + i);  }, 3000);',
+                upvotes: 20,
+                downvotes: 3,
+                user_id: 4,
+                question_id: 5
+            },
+            {
+                answer: 'function isPalindrome(str) {  str = str.replace(/\W/g, \'\').toLowerCase();  return (str == str.split(\'\').reverse().join(\'\'));}',
+                upvotes: 10,
+                downvotes: 3,
+                user_id: 4,
+                question_id: 6
+            },
+            {
+                answer: 'function palindrome(str) { var re = /[^A-Za-z0-9]/g; str = str.toLowerCase().replace(re, \'\'); var len = str.length; for (var i = 0; i < len/2; i++) {   if (str[i] !== str[len - 1 - i]) {       return false;   } } return true;}',
+                upvotes: 7,
+                downvotes: 2,
+                user_id: 4,
+                question_id: 6
+            },
+            {
+                answer: 'I think it is the tortoise because slow and steady wins the race and because a tortoise is the only animal listed with 5 digits on its hand so it can count better than the others.',
+                upvotes: 8,
+                downvotes: 2,
+                user_id: 4,
+                question_id: 7
+            },
+            {
+                answer: 'I don\'t know.',
+                upvotes: 3,
+                downvotes: 2,
+                user_id: 4,
+                question_id: 8
+            },
+            {
+                answer: 'You get brown if you mix purple and yellow because purple is made up of the two primary colors blue and red. Yellow is the third primary color and if you mix all three primary colors, you always get brown.',
+                upvotes: 37,
+                downvotes: 2,
+                user_id: 4,
+                question_id: 8
+            }
+]);
+});
 };
